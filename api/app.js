0822bc46b57a20f68164bfc90f41d80a37922633
FaizalKhan-AT <faizalkhanfk148@gmail.com>
2023-03-29 21:45:29 +0530

    inital: folder setup and basic files

---
+++ api/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const compression = require("compression");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const path = require("path");
+//middlewares
+app.use(compression());
+app.use(express.json());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(cors());
+
+const startServer = async () => {
+  try {
+    // await connectDB(process.env.MONGO_URI);
+    app.listen(
+      process.env.PORT,
+      console.log(`server and database started running at ${process.env.PORT} `)
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+startServer();
